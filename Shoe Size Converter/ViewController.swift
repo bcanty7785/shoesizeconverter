627f5806faba047f0bd0f45672d14f32addd2bf3
briancanty <brian_canty@mba.berkeley.edu>
2015-02-19 20:23:00 -0800

    Initial Commit

---
+++ Shoe Size Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shoe Size Converter
+//
+//  Created by Brian Canty on 2/19/15.
+//  Copyright (c) 2015 Brian Canty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
